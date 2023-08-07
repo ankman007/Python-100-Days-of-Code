fe0bc858ec75aebc8463f97f2db01332887b039c
ankit007 <ankeetpoudel6@gmail.com>
2023-08-07 21:49:06 +0545

    Formatting Commit

--- Beginner/Day-2-Tip-Calculator.py
+++ Beginner/Day-2-Tip-Calculator.py
@@ -1,9 +1,9 @@
 print('Welcome to the tip calculator')
-totalBill = float(input('What was the total bill? $'))
-tipPercentage = int(input('What percentage tip would you like to give? 10, 12, or 15? \n'))
-noOfPeople = int(input('How many people to split the bill?\n'))
+total_bill = float(input('What was the total bill? $'))
+tip_percentage = int(input('What percentage tip would you like to give? 10, 12, or 15? \n'))
+no_of_people = int(input('How many people to split the bill?\n'))
 
-totalTip = totalBill * (tipPercentage/100)
-billPerPerson = float((totalBill + totalTip)/noOfPeople)
+total_tip = total_bill * (tip_percentage/100)
+bill_per_person = float((total_bill + total_tip)/no_of_people)
 
-print(f'Each person should pay: ${billPerPerson}')
+print(f'Each person should pay: ${bill_per_person}')
