ea2d9d746fde7ed4daed43c9e81bb4a5f039ce1a
ankit007 <ankeetpoudel6@gmail.com>
2023-08-13 10:11:29 +0545

    Function Update

---
+++ Beginner/Day-8-Caesar-Cipher.py
@@ -0,0 +1,24 @@
+from Modules import PyArt, Data
+
+print(PyArt.caesar_cypher)
+print(Data.alphabet)
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
+text = input("Type your message: ").lower()
+shift = int(input("Type the shift number: "))
+
+
+def caesar(user_text, shift_amount, cipher_direction):
+    end_text = ''
+    if cipher_direction == 'decode':
+        shift_amount *= -1
+    for letter in user_text:
+        position = Data.alphabet.index(letter)
+        new_position = position + shift_amount
+        end_text += Data.alphabet[new_position]
+
+    print(f'\nThe {cipher_direction}d text is {end_text}.')
+
+
+caesar(text, shift, direction)
+
