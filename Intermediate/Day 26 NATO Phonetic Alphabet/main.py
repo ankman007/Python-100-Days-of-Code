4ccef13c620422782c0d857459c8ed4e0e8b9ead
ankit007 <ankeetpoudel6@gmail.com>
2023-09-08 17:57:02 +0545

    .

---
+++ Intermediate/Day 26 NATO Phonetic Alphabet/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv('nato_phonetic_alphabet.csv')
+
+phonetic_dict = {row.code: row.letter for (index, row) in df.iterrows()}
+
+word = input('Enter a word: ').upper()
+output_list = [phonetic_dict[letter] for letter in word]
+
+print(output_list)
+
+
+
--- Intermediate/Day 26/main.py
+++
@@ -1,5 +0,0 @@
-names = ['ankit', 'hari', 'phony', 'lappy', 'pee', 'su', 'ling']
-
-new_list = [n.upper() for n in names if len(n) > 3]
-
-print(new_list)
